24fc0f3c4d72b3476728c035ccdc3f457c3f3d01
Jordan Yoon-Buck <yoonbuck@uw.edu>
2019-10-13 00:32:33 -0700

    Pull and preprocess all data early for snappier use

--- src/component/MapView.js
+++ src/component/MapView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { API_KEY } from "../config/google.json";
 
 import "./MapView.css";
@@ -9,31 +9,14 @@ class MapView extends Component {
     super(props);
 
     this.onLoad = this.onLoad.bind(this);
-    this.newBounds = this.newBounds.bind(this);
-    this.newBoundsTimeout = this.newBoundsTimeout.bind(this);
   }
 
   onLoad(map) {
     this.$map = map;
 
     map.setZoom(14);
-    /*global google*/
+    /* global google */
     map.setCenter(new google.maps.LatLng(47.65, -122.3));
-
-    map.addListener("zoom_changed", this.newBoundsTimeout);
-  }
-
-  newBoundsTimeout() {
-    if (this.newBoundsTimeoutId) {
-      clearTimeout(this.newBoundsTimeoutId);
-    }
-    this.newBoundsTimeoutId = setTimeout(this.newBounds, 500);
-  }
-
-  newBounds() {
-    const bounds = this.$map.getBounds();
-    this.props.updateBounds(bounds);
-    this.$map.panToBounds(bounds);
   }
 
   render() {
@@ -42,7 +25,6 @@ class MapView extends Component {
         <LoadScript id="script-loader" googleMapsApiKey={API_KEY}>
           <GoogleMap
             onLoad={this.onLoad}
-            onDragEnd={this.newBounds}
             id="main-map"
             mapContainerStyle={{
               height: "100%",
